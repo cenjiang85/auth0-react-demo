908359a20cf53055543d60bb4c0894d0140fdd8a
sjiang <steve.jiang@comparethemarket.com.au>
2018-04-04 17:20:23 +1000

    Configured app

---
+++ src/Auth/auth0-variables.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+    domain: 'kdoggz.au.auth0.com',
+    clientId: 'htaEIl3ZrzeREwQkXUXV4Cmp5Na4vdnF',
+    callbackUrl: 'http://localhost:3000/callback',
+    apiUrl: 'http://localhost:3001'
+}
